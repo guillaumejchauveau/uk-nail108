7ebd953de8b4a74f4f066310e9e562005c535686
Guillaume Chauveau <guillaumejchauveau.ops.gecu@outlook.com>
2020-05-20 20:37:26 +0200

    Day 2

--- src/lab_node.cpp
+++ src/lab_node.cpp
@@ -1,13 +1,24 @@
 #include <ros/ros.h>
-#include "lab/RosPackageTemplate.hpp"
+#include <sensor_msgs/LaserScan.h>
+#include <algorithm>
 
-int main(int argc, char** argv)
-{
-  ros::init(argc, argv, "ros_package_template");
+void cb(const sensor_msgs::LaserScanConstPtr &msg) {
+  ROS_INFO_STREAM("Range:" << *std::min_element(msg->ranges.begin(), msg->ranges.end()));
+}
+
+int main(int argc, char **argv) {
+  ros::init(argc, argv, "lab");
   ros::NodeHandle nodeHandle("~");
 
-  lab::RosPackageTemplate rosPackageTemplate(nodeHandle);
-  //husky_highlevel_controller::HuskyHighlevelController huskyHighlevelController(nodeHandle);
+  std::string topic;
+  if (!nodeHandle.getParam("topic", topic)) {
+    ROS_ERROR("Could not find 'topic' parameter");
+  }
+  int queue_size;
+  if (!nodeHandle.getParam("topic_queue_size", queue_size)) {
+    ROS_ERROR("Could not find 'topic_queue_size' parameter");
+  }
+  //auto sub = nodeHandle.subscribe(topic, queue_size, &cb);
 
   ros::spin();
   return 0;
